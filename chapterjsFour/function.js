6c6564bdf72f785d5314ddee9bce3ac365e23c0e
Adetoye Babalola <adetoyebamise@gmail.com>
2021-03-12 12:04:54 +0100

    changes: added comments

--- chapterjsFour/function.js
+++ chapterjsFour/function.js
@@ -1,17 +1,22 @@
-
+// Declared a function and gave it a name
+// Named the parameter
 function workItOut(parameterFirst, parameterSecond, parameterThird)
     {
-    if (parameterThird == 'multiply'){
-      let result= parameterFirst*parameterSecond;
+      // Set a condition and assign the string value 'divide' the third parameter
+    if (parameterThird == 'divide'){
+       let  result= parameterFirst/parameterSecond;
   return result
-  } else if (parameterThird == 'divide'){
-        result= parameterFirst/parameterSecond;
-  return result
-  }
+  // Set a condition and assign the string value 'multiply' the third parameter
+  }else if (parameterThird == 'multiply'){ 
+      let result= parameterFirst*parameterSecond; 
+      return result}
+  // Set a condition and assign the string value 'sum' the third parameter
     else if (parameterThird == 'sum'){
-      result=parameterFirst+parameterSecond;
+      let result=parameterFirst+parameterSecond;
     return result
     } 
   }
+  // Call the function by simply writing out the name and stating the arguement
  workItOut(35,5,'divide');
+ 
                         
