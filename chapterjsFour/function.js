520ddcafb52501667f8140c32928f095bdf76427
Adetoye Babalola <adetoyebamise@gmail.com>
2021-03-12 11:20:31 +0100

    Changes: removed a trivial keyword

--- chapterjsFour/function.js
+++ chapterjsFour/function.js
@@ -1,13 +1,3 @@
-//Write a function that expects 3 arguments when called
-
-// The first two arguments should be numbers
-// The third argument should be a string
-// If the value of the third argument is ‘multiply’
-// Your function should return the multiplication of the first two arguments
-// If the value of the third argument is ‘divide
-// Your function should return the value of the first argument divided by the second argument
-// If the value of the third argument is ‘sum’
-// Your function should return the sum of the first two arguments
 
 function workItOut(parameterFirst, parameterSecond, parameterThird)
     {
@@ -23,5 +13,5 @@ function workItOut(parameterFirst, parameterSecond, parameterThird)
     return result
     } 
   }
-console.log('Let',+ workItOut(35,5,'divide'));
-workItOut(34,56,'sum');                           
+ workItOut(35,5,'divide');
+                        
