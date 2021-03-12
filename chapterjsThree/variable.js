d707ba041c53529aa34be7803087a24bb477b7d2
Adetoye Babalola <adetoyebamise@gmail.com>
2021-03-12 01:21:13 +0100

    changes: commented out some part of the code

--- chapterjsThree/variable.js
+++ chapterjsThree/variable.js
@@ -1,26 +1,26 @@
 //Declaring variable and assigning a string value
 var name = 'Adetoye'; //semicolon is optional
 var sayName = 'My name is Adetoye Babalola'
-// console.log(name);
+console.log();
 
 //Declaring a variable to store regular number
-var number =12;
-var integer = 3.1472312323;
-var negNumber = -1;
-var negInteger = -3.14723123;
+// var number =12;
+// var integer = 3.1472312323;
+// var negNumber = -1;
+// var negInteger = -3.14723123;
 
 //Boolean data type tells us if a variable is true or false
-var weShallSetForthAtDawn = true;
-var theSunWillRiseAgain = true;
-var iCannotThinkOfAnyFalseStatement = false;
+// var weShallSetForthAtDawn = true;
+// var theSunWillRiseAgain = true;
+// var iCannotThinkOfAnyFalseStatement = false;
 
 //Declaring a variable null that stores the intentional absence of a data
 
-var emptyInside = null;
+// var emptyInside = null;
 
 //Declaring a variable that is not defined - undefined
-var ;
+// var ;
 
 //Declaring a variable known as symbol: identifiers for object property
-Symbol()
+// Symbol()
 
