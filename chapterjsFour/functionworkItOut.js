49719bf0790b1649681091e746140b8385f5f551
Adetoye Babalola <adetoyebamise@gmail.com>
2021-03-15 08:50:24 +0100

    indent some lines

--- chapterjsFour/functionworkItOut.js
+++ chapterjsFour/functionworkItOut.js
@@ -1,23 +1,32 @@
-// Declared a function and gave it a name
-// Named the parameter
-function workItOut(parameterFirst, parameterSecond, parameterThird)
-    {
-      // Set a condition and assign the string value 'divide' the third parameter
+  // Declared a function and gave it a name
+  // Named the parameter
+
+function workItOut ( parameterFirst, parameterSecond, parameterThird ) {
+
+  // Set a condition and assign the string value 'divide' the third parameter
+
     if (parameterThird == 'divide'){
        let  result = parameterFirst/parameterSecond;
-  return result
-  // Set a condition and assign the string value 'multiply' the third parameter
-  }else if (parameterThird == 'multiply'){ 
+    return result
+      }
+  // Set a condition and assign the string value 'multiply' the third 
+  
+   else if (parameterThird == 'multiply'){ 
       let result= parameterFirst*parameterSecond; 
-      return result}
+   return result
+      }
+
   // Set a condition and assign the string value 'sum' the third parameter
+
     else if (parameterThird == 'sum'){
       let result=parameterFirst+parameterSecond;
     return result
     } 
   }
   // Call the function by simply writing out the name and stating the arguement
- workItOut(35,5,'divide');
+ 
+ 
+  console.log(workItOut(35,5,'multiply'));
  
 
  
