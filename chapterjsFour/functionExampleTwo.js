8438e316316e58a235f97d263eb32684538e9320
Adetoye Babalola <adetoyebamise@gmail.com>
2021-03-15 07:46:40 +0100

    indent code for readability

--- chapterjsFour/functionExampleTwo.js
+++ chapterjsFour/functionExampleTwo.js
@@ -1,7 +1,9 @@
 //Example two should be on an operator
+
     function squareNumber(number){
         let result = number * number
       return result
 
     }
-     squareNumber();
+     
+    squareNumber();
