a10772506de531e4e7cbc115c6b8e7ced864fc0b
Adetoye Babalola <adetoyebamise@gmail.com>
2021-03-16 08:52:12 +0100

    rearrange code

--- chapterjsFour/realisticArraysExample.js
+++ chapterjsFour/realisticArraysExample.js
@@ -1,3 +1,22 @@
-const music = [ 'Hiphop', 'Rock', 'Soul', 'R&B', 'Gospel', 'Fuji', 'Tungba']
-const continent = ['Africa', 'Asia', 'Antarctica', 'North America', 'South America', 'Europe', 'Oceania' ]
-const pet = ['dog', 'cat', 'snake', 'bird']
+// Create 3 realistic arrays with at least 3 other realistic arrays as elements
+
+ const music = [[ 'Hiphop', 'Rock', 'Soul', 'R&B', 'Gospel', 'Fuji', 'Tungba'],
+                ['Michael Jackson', 'Bob Marley', 'Wizkid', 'Davido'], 
+                ['Mercury Award', 'Grammy Award', 'Peoples choice Award'] ]  
+
+
+ const continent = [['Africa', 'Asia', 'Antarctica', 'North America', 'South America',
+                     'Europe', 'Oceania' ], 
+                    ['North Africa', 'West Africa', 'Central Africa', 'East Africa', 'South Africa',]
+                    ['North Korea', 'Japan' ]
+                    ['']
+                    ['Canada', 'United States of America']
+                    ['Altiplano', 'Amazon Basin', 'Andes', 'Carribean South America']
+                    ['Eastern Europe','NorthEastern Europe','Northern Europe']
+                    ['Papua New Guinea','New Zealand','Pacific']]
+
+
+ const pet = [['dog', 'cat', 'bird'],
+              ['Bull Dog', 'German Sheperd', 'Labrador Retriever', 'Golden Retreiver', 'Poodle'],
+              ['Persian cat', 'British shorthair', 'Maine Coon', 'Bengal cat' ],
+              ['Red factor canary', 'Dove', 'parrotlet', 'cockatoo', 'cockatiel']]
