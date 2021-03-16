bc1a665474c40b8171831064c0be1db32a0fd2be
Adetoye Babalola <adetoyebamise@gmail.com>
2021-03-16 09:02:28 +0100

    rearrange code

--- chapterjsFour/arrayWithObjectsAsElements.js
+++ chapterjsFour/arrayWithObjectsAsElements.js
@@ -1,14 +1,14 @@
 // Create an array with two realistic objects as its elements
 
  const city = [ 'Lusaka', 'Lagos' = {
-                            region: '',
-                            weatherCondition : '',
-                            numberofIslands : '',
+                            region: 'Subsaharan Africa',
+                            weatherCondition : 'low Humidity',
+                            numberofIslands : 4,
 
             }, 'Kigali', 'sao Tome', 'Moscow' ={
-                                       region: '',
-                                       weatherCondition: '',
-                                       numbersOfIslands : '',
+                                       region: 'West Russia',
+                                       weatherCondition: 'Koppen climate',
+                                       numbersOfIslands : 65,
 
             }, 'Dubai']
 
