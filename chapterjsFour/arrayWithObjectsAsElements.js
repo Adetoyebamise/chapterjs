e36a90731c822b0443e1c2d3f359eb71b2cddd4e
Adetoye Babalola <adetoyebamise@gmail.com>
2021-03-16 03:45:30 -0700

    add new element to object

--- chapterjsFour/arrayWithObjectsAsElements.js
+++ chapterjsFour/arrayWithObjectsAsElements.js
@@ -4,11 +4,13 @@
                             region: 'Subsaharan Africa',
                             weatherCondition : 'low Humidity',
                             numberofIslands : 4,
+                            geographicCoordinates : "10 00N, 8 00E" 
 
             }, 'Kigali', 'sao Tome', 'Moscow' ={
                                        region: 'West Russia',
                                        weatherCondition: 'Koppen climate',
                                        numbersOfIslands : 65,
+                                       geographicCoordinates: "60 00N, 100 00E"
 
             }, 'Dubai']
 
