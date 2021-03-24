f140de8d37ef059aa1994b91378175ae3de378db
Adetoye Babalola <adetoyebamise@gmail.com>
2021-03-24 07:54:38 +0100

    initial commit

---
+++ chapterjsNineLoop/arrayOfObjectAndFunction.js
@@ -0,0 +1,16 @@
+/* Create an array with three elements: The second element should be a function, and make the function usable outside the aray. */
+
+const holyBooks = [
+  { name: 'Quaran',
+    religion: 'Islam'
+  },
+
+  () => {
+    
+  },
+
+  {
+  name: 'Torah',
+  religion: 'Judaism'
+  }
+];
